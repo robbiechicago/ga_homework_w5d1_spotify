00623423f2456a9dd4376b3ed92afa9edb009f71
Rob Holloway <robbiechicago@gmail.com>
2015-09-07 19:26:31 +0100

    Created an artist search function, although its hard coded with Coldplay.  Nobody wants to search for coldplay...

--- js/script.js
+++ js/script.js
@@ -1,9 +1,51 @@
 $(document).ready(function() {
   console.log('Ready to go, RoHo.');
 
-  function search () {
+  var searchOptionArtist;
+  var searchOptionAlbum;
+  var searchOptionTrack;
+
+  function artistSearch () {
     $.get('https://api.spotify.com/v1/search?q=coldplay&type=artist', function(response) {
-      
-    }
-  }; //END OF FUNCTION SEARCH
+      var theArtists = response.artists.items;
+      console.log(theArtists);
+      $.each(theArtists, function(index, value) {
+        console.log(value.name);
+      });
+      // debugger
+    });
+  }; //END OF FUNCTION ARTIST_SEARCH
+
+  search();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }); //END OF DOCUMENT READY
+
+
